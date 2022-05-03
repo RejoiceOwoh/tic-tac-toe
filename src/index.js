4769099755ff8b3bb9d7a6c7f3e26402ec1baf5c
Owoh Rejoice Chidera <owohrejoice@gmail.com>
2022-05-03 11:18:46 +0100

    Still working on some props

--- src/index.js
+++ src/index.js
@@ -3,19 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         value: null,
+      }
+   }
+
+
    render() {
      return (
-       <button className="square" onClick={function() {console.log('click'); }} >
+       <button className="square" onClick={() => {this.setState({value: 'X'}) }} >
          {/* TODO */}
-         {this.props.value}
+         {this.state.value}
        </button>
      );
    }
  }
  
  class Board extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       squares: Array(9).fill(null),
+     };
+   }
    renderSquare(i) {
-     return <Square value={i} />;
+     return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+     );
    }
  
    render() {
